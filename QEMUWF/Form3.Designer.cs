e943c278caf806ae10f95d8c1fa4dbf064be8fec
ojask <techno.mind69@gmail.com>
2023-05-03 17:37:00 +0530

    alignment fiz

--- QEMUWF/Form3.Designer.cs
+++ QEMUWF/Form3.Designer.cs
@@ -229,7 +229,7 @@ namespace QEMUWF
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Location = new System.Drawing.Point(9, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(398, 32);
             this.label1.TabIndex = 0;
@@ -539,7 +539,7 @@ namespace QEMUWF
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(10, 9);
+            this.label11.Location = new System.Drawing.Point(9, 7);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(312, 32);
             this.label11.TabIndex = 1;
